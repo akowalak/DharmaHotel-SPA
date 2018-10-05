3430c0c158c21da073d9ef4cfc883e4ee81c2622
Anna Kowalak <akowalak89@gmail.com>
2018-10-05 19:05:59 +0200

    function that closes the lightbox

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -2,3 +2,8 @@ function openModal() {
   document.getElementById('myModal').style.display = "block";
 }
 
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+
